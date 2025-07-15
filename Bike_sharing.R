a3405fb921b539fc65a649f98055aece48daa650
Supreet Mutsuddi <sureetmutsuddi@gmail.com>
2025-07-15 11:20:33 -0700

    change

--- Bike_sharing.R
+++ Bike_sharing.R
@@ -1,7 +1,5 @@
 library(tidyverse)
 
-
-
 #drop features
 
 df <- hour
@@ -31,24 +29,49 @@ df %>%
 
 #Univariate Analysis
 
-#Convert to factors
+#Convert relevant predictors to factors
 
 df <- df %>%
   mutate(across(c('season', 'yr', 'mnth', 'hr', 'holiday', 'weekday', 'workingday',
            'weathersit'), as.factor))
 
+#Check continuous predictors
 
+df %>%
+  select(temp, atemp, hum, windspeed) %>%
+  summary()
 
+df %>%
+  select(temp, atemp, hum, windspeed) %>%
+  gather(variable, value) %>%
+  ggplot(aes(x = variable, y = value)) +
+  geom_boxplot(fill = 'skyblue') +
+  labs(title = 'Boxplot of continuous predictors')
 
+#Check for missing values
 
+colSums(is.na(df))
 
+#Check categorical predictors
 
+table(df$season)
+table(df$yr)
+table(df$mnth)
+table(df$hr)
+table(df$holiday)
+table(df$weekday)
+table(df$workingday)
+table(df$weathersit)
 
+#Combine levels
 
+df$weathersit <- as.character(df$weathersit)
+df$weathersit[df$weathersit == 4] <- '3'
+df$weathersit <- as.factor(df$weathersit)
 
+################################################################################
 
-
-
+#Multivariate Analysis
 
 
 

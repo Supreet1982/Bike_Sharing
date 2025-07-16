acdd2c338da5256f14b64862a6fb1eaa9820dad5
Supreet Mutsuddi <sureetmutsuddi@gmail.com>
2025-07-16 09:37:04 -0700

    change

--- Bike_sharing.R
+++ Bike_sharing.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(car)
+library(GGally)
 
 #drop features
 
@@ -81,8 +83,6 @@ df$workingday <- factor(df$workingday, levels = c(0, 1),
 df$weathersit <- factor(df$weathersit, levels = c(1, 2, 3),
               labels = c('Clear', 'Misty_Cloudy', 'Light_Rain_Snow'))
 
-
-
 #Relevel categorical variables
 
 cat_vars <- c('season', 'yr', 'mnth', 'hr', 'holiday', 'weekday',
@@ -95,13 +95,81 @@ for (i in cat_vars) {
   df[,i] <- relevel(df[,i], ref = level_name)
 }
 
+df %>%
+  ggplot(aes(weathersit)) +
+  geom_bar(fill='blue')
 
 ################################################################################
 
 #Multivariate Analysis
 
+#Numeric predictors vs Numeric target
+
+df %>%
+  ggplot(aes(temp, cnt)) +
+  geom_point(alpha=0.6) +
+  geom_smooth(method = 'loess', color='blue') +
+  labs(title = 'Bike Rentals vs Temperature', x = 'Normalized Temp',
+       y = 'Rentals (cnt)')
+
+df %>%
+  ggplot(aes(hum, cnt)) +
+  geom_point(alpha=0.6) +
+  geom_smooth(method = 'loess', color='blue') +
+  labs(title = 'Bike Rentals vs Humidity', x = 'Normalized Humidity',
+       y = 'Rentals (cnt)')
+
+df %>%
+  ggplot(aes(windspeed, cnt)) +
+  geom_point(alpha=0.6) +
+  geom_smooth(method = 'loess', color='blue') +
+  labs(title = 'Bike Rentals vs Windspeed', x = 'Normalized Windspeed',
+       y = 'Rentals (cnt)')
+
+#Categorical predictors vs Numeric target
+
+df %>%
+  group_by(hr) %>%
+  summarize(avg_cnt = mean(cnt)) %>%
+  ggplot(aes(x = as.numeric(as.character(hr)), y = avg_cnt)) +
+  geom_line() +
+  labs(title = 'Average Rentals by Hour', x = 'Hour', y = 'Average Rentals')
+
+df %>%
+  group_by(hr, workingday) %>%
+  summarize(avg_cnt = mean(cnt)) %>%
+  ggplot(aes(x = as.numeric(as.character(hr)), y = avg_cnt,
+             color = factor(workingday))) +
+  geom_line() +
+  labs(title = 'Hourly Rentals: Working Day vs Non-working Day', x = 'Hour', 
+       y = 'Average Rentals', color = 'Working Day')
+
+df %>%
+  ggplot(aes(season, cnt)) +
+  geom_boxplot(fill = 'skyblue') +
+  labs(title = 'Bike Rentals by Season', x = 'Season', y = 'Rentals (cnt)')
+
+df %>%
+  ggplot(aes(x = hum, y = cnt, color = weathersit)) +
+  geom_point() +
+  geom_smooth(method = 'lm', se = FALSE)
 
 
+#Multicollinearity check
+
+num_vars <- df %>% select(temp, atemp, hum, windspeed)
+cor(num_vars)
+
+ggpairs_plot <- ggpairs(df %>% select(cnt, temp, atemp, hum, windspeed))
+
+ggsave('corr_plot.png', plot = ggpairs_plot, width = 10, height = 8, dpi = 300,
+       units = 'in')
+
+vif(lm(cnt ~ temp + hum + windspeed, data = df))
+
+df$atemp <- NULL
+
+################################################################################
 
 
 
